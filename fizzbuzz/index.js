5bb1db2881d39f5f10fcd71fa2e3bb295e427636
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-07-14 19:51:25 -0700

    feat: add jest

---
+++ fizzbuzz/index.js
@@ -0,0 +1,20 @@
+const fizzBuzz = (number) => {
+  let divByThree = false;
+  let divByFive = false;
+
+  if (number % 3 === 0) divByThree = true;
+  if (number % 5 === 0) divByFive = true;
+
+  if (divByThree && divByFive) return 'fizzbuzz';
+  else if (divByThree) return 'fizz';
+  else if (divByFive) return 'buzz';
+  else return number;
+};
+
+const fizzBuzzRunner = (number) => {
+  for (let i = 1; i <= number; i++) {
+    console.log(fizzBuzz(i));
+  }
+};
+
+module.exports = fizzBuzzRunner;
